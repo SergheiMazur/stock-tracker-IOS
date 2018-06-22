ec559927ba98ba434ef7a8e9de9569ec41db1eb3
Serghei Mazur <serhio1007@gmail.com>
2018-06-21 21:35:01 -0400

    Initial Commit

---
+++ Stock Tracker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Stock Tracker
+//
+//  Created by Serghei Mazur on 2018-06-21.
+//  Copyright © 2018 Serghei Mazur. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
